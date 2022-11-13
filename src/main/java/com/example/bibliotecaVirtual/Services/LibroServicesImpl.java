f1557794361666977823d638a833fa48e6535baa
sebastian.suarez <juan.suarez@o365.unab.edu.co>
2022-11-13 16:00:20 -0500

    Se corrigió Repository

--- src/main/java/com/example/bibliotecaVirtual/Services/LibroServicesImpl.java
+++ src/main/java/com/example/bibliotecaVirtual/Services/LibroServicesImpl.java
@@ -3,68 +3,129 @@ package com.example.bibliotecaVirtual.Services;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.example.bibliotecaVirtual.model.Libro;
+import com.example.bibliotecaVirtual.repository.LibroRepository;
 
 public class LibroServicesImpl implements LibroServices {
 
+	@Autowired
+	LibroRepository repository;
+	
 	@Override
 	public Optional<Libro> buscarLibroNoAnyo(Long id, String titulo, String autor, String editorial, String tipo) {
-		// TODO Auto-generated method stub
-		return Optional.empty();
+		if(id == null && titulo == null && autor == null && editorial == null && tipo == null) {
+			return Optional.empty();
+		}
+		if(id == null) {
+			List<Libro> libros = repository.findByTitulo(titulo);
+			if(libros.isEmpty()) {
+				return Optional.empty();
+			}else {
+				return Optional.of(libros.get(0));
+			}
+		}
+		if(titulo == null) {
+			return repository.findByAutor(autor);
+		}
+		List<Libro> librosFiltrados = repository.findByAllNotAnyo(id, titulo, autor, editorial, tipo);
+		if(librosFiltrados.isEmpty()) {
+			return Optional.empty();
+		}else {
+			return Optional.of(librosFiltrados.get(0));
+		}
 	}
 
 	@Override
 	public Optional<Libro> buscarPorId(Long id) {
-		// TODO Auto-generated method stub
-		return Optional.empty();
+		if(id == null) {
+			return Optional.empty();
+		}
+		return repository.findById(id);
+		
 	}
 
 	@Override
 	public Optional<Libro> buscarPorTitulo(String titulo) {
-		// TODO Auto-generated method stub
-		return Optional.empty();
+		if(titulo == null) {
+			return Optional.empty();
+		}
+		return repository.findByTitulo(titulo);
 	}
 
 	@Override
 	public Optional<Libro> buscarPorAutor(String autor) {
-		// TODO Auto-generated method stub
-		return Optional.empty();
+		if(autor == null) {
+			return Optional.empty();
+		}
+		return repository.findByAutor(autor);
 	}
 
 	@Override
 	public Optional<Libro> buscarPorEditorial(String editorial) {
-		// TODO Auto-generated method stub
-		return Optional.empty();
+		if(editorial == null) {
+			return Optional.empty();
+		}
+		return repository.findByEditorial(editorial);
 	}
 
 	@Override
 	public Optional<Libro> buscarPorTipo(String tipo) {
-		// TODO Auto-generated method stub
-		return Optional.empty();
+		if(tipo == null) {
+			return Optional.empty();
+		}
+		return repository.findByTipo(tipo);
 	}
 
 	@Override
 	public List<Libro> buscarTodos() {
-		// TODO Auto-generated method stub
-		return null;
+		return repository.findAll();
 	}
 
 	@Override
 	public Libro crearLibro(Libro libro) {
-		// TODO Auto-generated method stub
-		return null;
+		if(libro == null) {
+			return null;
+		}
+		if(libro.getId() == null) {
+			return null;
+		}
+		Optional<Libro> libroExistente = repository.findById(libro.getId());
+		if(libroExistente.isPresent()) {
+			return libroExistente;
+		}else {
+			return repository.insert(libro);
+		}
 	}
 
 	@Override
 	public Libro actualizarLibro(Libro libro) {
-		// TODO Auto-generated method stub
-		return null;
+		if(libro == null) {
+			return null;
+		}
+		if(libro.getId() == null) {
+			return null;
+		}
+		Optional<Libro> libroExistente = repository.findById(libro.getId());
+		if(libroExistente.isPresent()) {
+			return repository.save(libro);
+		}else {
+			return null;
+		}
 	}
 
 	@Override
 	public String eliminarLibro(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		if(id == null) {
+			return  "El código no puede ser ejecutado;
+		}
+		Optional<Libro> libroExistente = repository.findById(id);
+		if(libroExistente.isPresent()) {
+			repository.delete(libroExistente.get());
+			return "El Libro ha sido eliminado";
+		}
+		return "El pais no pudo ser eliminado"
 	}
 
 }
--- src/main/java/com/example/bibliotecaVirtual/repository/LibroRepository.java
+++ src/main/java/com/example/bibliotecaVirtual/repository/LibroRepository.java
@@ -1,11 +1,32 @@
 package com.example.bibliotecaVirtual.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 import com.example.bibliotecaVirtual.model.Libro;
 
 @Repository
 public interface LibroRepository extends MongoRepository <Libro, Long> {
-
+	
+	@Query("{id: ?0, titulo: '?1', autor: '?2', editorial: '?3', tipo: '?4'}")
+	public List<Libro> findByAllNotAnyo(Long id, String titulo, String autor, String editorial, String tipo);
+	
+	@Query("{id: ?0}")
+	public Optional<Libro> findById(Long id);
+	
+	@Query("{titulo: '?0'}")
+	public List<Libro> findByTitulo(String titulo);
+	
+	@Query("{autor: '?0'}")
+	public List<Libro> findByAutor(String autor);
+	
+	@Query("{editorial: '?0'}")
+	public List<Libro> findByEditorial(String editorial);
+	
+	@Query("{tipo: '?0'}")
+	public List<Libro> findByTipo(String tipo);
 }
